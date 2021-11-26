0ef52c12b1a4dda647e4616a7f2d275a973e26de
dezske42 <andras.dezsoe@gmail.com>
2021-11-26 23:49:14 +0100

    Result of second session - solves first puzzle.

--- venv/src/sudoku.py
+++ venv/src/sudoku.py
@@ -2,6 +2,8 @@ def row_to_string(row):
     return " ".join([digit for digit in row])
 
 class Sudoku:
+    all_digits = frozenset([str(digit) for digit in range(1,10)])
+
     def __init__(self, s = None):
         self.rows = []
         for i in range(9):
@@ -30,3 +32,73 @@ class Sudoku:
         for r in range(3):
             square.update([digit for digit in self.rows[row*3+r][column*3:column*3+3]])
         return square - {' '}
+
+    def possible_at(self, row, column):
+        cell = self.at(row, column)
+        if cell == " ":
+            return self.all_digits - self.row_set(row) - self.column_set(column) - self.square_set(row//3,column//3)
+        else:
+            return set(cell)
+
+    def where_is_digit_possible_in_row(self, digit, row):
+        # just to speed up
+        index = self.rows[row].find(digit)
+        if index != -1:
+            return set([index])
+
+        # this is sufficient to pass unit tests
+        where = set()
+        for column in range(0, 9):
+            if digit in self.possible_at(row,column):
+                where.add(column)
+        return where
+
+    def where_is_digit_possible_in_column(self, digit, column):
+        where = set()
+        for row in range(0, 9):
+            if digit in self.possible_at(row,column):
+                where.add(row)
+        return where
+
+    def fill_only_possible_by_row(self, row):
+        for digit in self.all_digits - self.row_set(row):
+            digit_is_possible_at = self.where_is_digit_possible_in_row(digit, row)
+            if len(digit_is_possible_at) == 1:
+                column = next(iter(digit_is_possible_at))
+                self.rows[row]= self.rows[row][0:column] + digit + self.rows[row][column+1:]
+
+    def fill_only_possible_by_column(self, column):
+        progress = False
+        for digit in self.all_digits - self.column_set(column):
+            digit_is_possible_at = self.where_is_digit_possible_in_column(digit, column)
+            if len(digit_is_possible_at) == 1:
+                row = next(iter(digit_is_possible_at))
+                self.rows[row]= self.rows[row][0:column] + digit + self.rows[row][column+1:]
+                progress = True
+        return progress
+
+if __name__ == '__main__':
+    puzzle = ("1   4   8 6 5    "
+              "8             7 6"
+              "3       7        "
+              "  8 7   6   3    "
+              "6 4 2 7   5 1 9 8"
+              "    3   4   7 6  "
+              "        2       1"
+              "4 6             5"
+              "    5 6 1   9   7")
+    sudo = Sudoku(puzzle)
+    print(str(sudo) + "\n")
+    anyProgress = True
+    while anyProgress:
+        anyProgress = False
+        for row in range(0, 9):
+            progress = sudo.fill_only_possible_by_row(row)
+            if progress:
+                print(str(sudo) + "\n")
+                anyProgress = True
+        for column in range(0, 9):
+            progress = sudo.fill_only_possible_by_column(column)
+            if progress:
+                print(str(sudo) + "\n")
+                anyProgress = True
--- venv/test/sudoku_test.py
+++ venv/test/sudoku_test.py
@@ -23,6 +23,8 @@ puzzle1 = ( "1   4   8 6 5    "
             "4 6             5"
             "    5 6 1   9   7")
 
+all_digits = frozenset([str(digit) for digit in range(1, 10)])
+
 class MyTestCase(unittest.TestCase):
     def test_can_print_board(self):
         sudoString =      ("1 2 3 4 5 6 7 8 9\n"
@@ -62,7 +64,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_first_row_is_full(self):
         sudo = Sudoku(fullInitializer)
-        self.assertEqual(sudo.row_set(0), set([str(digit) for digit in range(1,10)]))
+        self.assertEqual(sudo.row_set(0), all_digits)
 
     def test_last_row_is_empty(self):
         emptySudo = Sudoku()
@@ -70,7 +72,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_last_row_is_full(self):
         sudo = Sudoku(fullInitializer)
-        self.assertEqual(sudo.row_set(0), set([str(digit) for digit in range(1,10)]))
+        self.assertEqual(sudo.row_set(0), all_digits)
 
     def test_first_row_of_puzzle1(self):
         sudo = Sudoku(puzzle1)
@@ -86,7 +88,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_first_column_is_full(self):
         sudo = Sudoku(fullInitializer)
-        self.assertEqual(sudo.column_set(0), set([str(digit) for digit in range(1,10)]))
+        self.assertEqual(sudo.column_set(0), all_digits)
 
     def test_first_column_of_puzzle1(self):
         sudo = Sudoku(puzzle1)
@@ -102,7 +104,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_top_left_square_is_full(self):
         sudo = Sudoku(fullInitializer)
-        self.assertEqual(sudo.square_set(0,0), set([str(digit) for digit in range(1,10)]))
+        self.assertEqual(sudo.square_set(0,0), all_digits)
 
     def test_top_left_square_of_puzzle1(self):
         sudo = Sudoku(puzzle1)
@@ -111,6 +113,78 @@ class MyTestCase(unittest.TestCase):
     def test_bottom_right_square_of_puzzle1(self):
         sudo = Sudoku(puzzle1)
         self.assertEqual(sudo.square_set(2,2), {"1", "5", "7", "9"})
+
+    def test_possible_at_top_left_of_empty(self):
+        sudo = Sudoku()
+        self.assertEqual(sudo.possible_at(0,0), all_digits)
+
+    def test_possible_at_top_left_of_puzzle1(self):
+        sudo = Sudoku(puzzle1)
+        self.assertEqual(sudo.possible_at(0,0), {"1"})
+
+    def test_possible_at_bottom_right_of_puzzle1(self):
+        sudo = Sudoku(puzzle1)
+        self.assertEqual(sudo.possible_at(8,8), {"7"})
+
+    def test_possible_at_0_1_of_puzzle1(self):
+        sudo = Sudoku(puzzle1)
+        self.assertEqual(sudo.possible_at(0,1), {"2", "7", "9"})
+
+    def test_possible_at_1_1_of_puzzle1(self):
+        sudo = Sudoku(puzzle1)
+        self.assertEqual(sudo.possible_at(1,1), {"2", "5", "9"})
+
+    def test_where_is_1_possbile_in_row_0_of_puzzle1(self):
+        sudo = Sudoku(puzzle1)
+        self.assertEqual(sudo.where_is_digit_possible_in_row("1", 0), {0})
+
+    def test_where_is_4_possbile_in_row_0_of_puzzle1(self):
+        sudo = Sudoku(puzzle1)
+        self.assertEqual(sudo.where_is_digit_possible_in_row("4", 0), {2})
+
+    def test_where_is_7_possbile_in_row_0_of_puzzle1(self):
+        sudo = Sudoku(puzzle1)
+        self.assertEqual(sudo.where_is_digit_possible_in_row("7", 0), {1})
+
+    def test_where_is_6_possbile_in_row_3_of_puzzle1(self):
+        sudo = Sudoku(puzzle1)
+        self.assertEqual(sudo.where_is_digit_possible_in_row("6", 3), {4})
+
+    def test_fill_only_possible_by_row_0_of_puzzle1(self):
+        sudo = Sudoku(puzzle1)
+        filled = Sudoku( "1 7 4   8 6 5    "
+                         "8             7 6"
+                         "3       7        "
+                         "  8 7   6   3    "
+                         "6 4 2 7   5 1 9 8"
+                         "    3   4   7 6  "
+                         "        2       1"
+                         "4 6             5"
+                         "    5 6 1   9   7")
+        sudo.fill_only_possible_by_row(0)
+        self.assertEqual(str(sudo), str(filled))
+
+    def test_fill_only_possible_by_row_0_of_puzzle1(self):
+        sudo =    Sudoku( "1 7 4   8 6 5    "
+                         "8             7 6"
+                         "3       7        "
+                         "  8 7   6   3    "
+                         "6 4 2 7   5 1 9 8"
+                         "    3   4   7 6  "
+                         "        2       1"
+                         "4 6             5"
+                         "    5 6 1   9   7")
+        filled = Sudoku( "1 7 4   8 6 5    "
+                         "8             7 6"
+                         "3       7        "
+                         "  8 7   6   3    "
+                         "6 4 2 7   5 1 9 8"
+                         "    3   4   7 6  "
+                         "7       2       1"
+                         "4 6             5"
+                         "2   5 6 1   9   7")
+        sudo.fill_only_possible_by_column(0)
+        self.assertEqual(str(sudo), str(filled))
 
 if __name__ == '__main__':
     unittest.main()
